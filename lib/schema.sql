d8b260aca94224a994e13180d67af2c02c353d06
Steven Senkowski <scsenkowski@gmail.com>
2017-12-26 14:01:53 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,5 +4,28 @@ CREATE TABLE characters (
     motto TEXT,
     species TEXT,
     author_id INTEGER,
-    series_id INTEGER,
-)
+    series_id INTEGER
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT
+  
+);
+
+CREATE TABLE sub_genre(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors(
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books(
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year DATE,
+  series_id
+);
